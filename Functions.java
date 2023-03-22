e1b2e7ff9e279b04a71d59321e1c52a25a82d998
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2023-03-22 23:07:10 +0530

    Create Functions.java

---
+++ Functions.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Function {
+public static void printHelloWorld(){
+System.out.println("Hello World");
+System.out.println("Hello World");
+System.out.println("Hello World");
+
+}
+    public static void main(String[] args) {
+    printHelloWorld(); //function call
+    }
+    
